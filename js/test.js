29763d37bd7e1354461f4f5b0656d614e36f5b6f
CalSch <35741152+CalSch@users.noreply.github.com>
2021-01-22 09:33:44 -0600

    Update test.js

--- js/test.js
+++ js/test.js
@@ -6,10 +6,6 @@ let h=`<html>
     `+document.body.innerHTML+`
   </body>
 </html>`;
-while (h.search("<") != -1 || h.search(">") != -1) {
-  h=h.replace("<", "&lt;");
-  h=h.replace(">", "&rt;");
-}
 let a=document.createElement("textarea");
 a.rows=30;
 a.cols=60;
