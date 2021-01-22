b42073b2c28d35c4b7479d789b20658eb2e239b2
CalSch <35741152+CalSch@users.noreply.github.com>
2021-01-22 09:32:46 -0600

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,18 @@
-alert("hello!");
+let h=`<html>
+  <head>
+    `+document.head.innerHTML+`
+  </head>
+  <body>
+    `+document.body.innerHTML+`
+  </body>
+</html>`;
+while (h.search("<") != -1 || h.search(">") != -1) {
+  h=h.replace("<", "&lt;");
+  h=h.replace(">", "&rt;");
+}
+let a=document.createElement("textarea");
+a.rows=30;
+a.cols=60;
+a.innerText=h;
+document.body.appendChild(a);
+alert("Done!");
