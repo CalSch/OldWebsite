da24c54d72eafbd2b10a2a6c1c4e2112899f83f8
CalSch <35741152+CalSch@users.noreply.github.com>
2021-02-12 10:30:17 -0600

    Update JsUtil.js

--- js/JsUtil.js
+++ js/JsUtil.js
@@ -1,9 +1,23 @@
 alert("No syntax error!");
 try {
 
-  var choice=prompt("
+	function testJQ(){
+		if (typeof($) === undefinied){
+			alert("Could not find JQuery");
+		} else {
+			alert("JQuery found!");
+		}
+	}
+	
+	var choice=prompt(`Welcome to the JavaScript Util!
+1: Test for JQuery`);
+	switch (choice){
+		case "1":
+			testJQ();
+			break;
+	}
 
 }
 catch(e) {
- alert("Error: "+e.message);
+	alert("Error: "+e.message);
 }
